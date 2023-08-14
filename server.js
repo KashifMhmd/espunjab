ac763996c63b748a2812efa5cf02c0a8dc25cfc3
KashifMhmd <98969010+KashifMhmd@users.noreply.github.com>
2023-08-14 14:57:17 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -24,9 +24,11 @@ app.use('/api-docs', require('_helpers/swagger'));
 
 // global error handler
 app.use(errorHandler);
-
+app.use('/',(req,res)=>{
+    res.send('welcom')
+})
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
